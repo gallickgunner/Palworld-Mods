3ddfc1a242ab937856d460a4b396ffe0d8bb2606
gallickgunner <ahmed.u@outlook.com>
2026-08-26 00:12:39 +0500

    update randomized sized pals
    - Found a better stable hook which initialized values of IsBossPal, isTowerBoss etc correctly. Previous hook was faulty and boss pals were passing through!
    
    - Added a deliberate delay before applying offset so that my possible future scale mods can run before this one. Also any other scale mod that apply scale immediately would get the size offset on top of their modified scale.
    
    - Removed config file from current and future updates. Mod will generate a config file on first startup if not found. This allows user config file to persist instead of getting overwritten by steam workshop updates

---
+++ RandomSizedPals/Scripts/config.lua
@@ -0,0 +1,48 @@
+-- RandomSizedPals
+--
+-- SizeVariation values are ADDITIVE offsets from the Pal's native value for
+-- DefaultScale3D.
+--
+-- Example:
+--   XS = { min = 0.0, max = 0.5 }
+--   native 1.0 -> final 1.0 .. 1.5
+--
+--   XL = { min = -0.5, max = 0.0 }
+--   native 4.0 -> final 3.5 .. 4.0
+--
+-- These offsets can also be used to downscale Pals belonging to a specific category by setting max < 0.
+-- For e.g, setting XL = { min = -0.5, max = -0.2} will scale all Pals in XL category 
+-- such that they fall in the range 0.5 - 0.8 as the default scale for normal non-boss pals is 1.0.
+--
+-- Likewise, you can also use the opposite setting to upscale native Pals by setting min > 0
+-- In any case, just make sure that "min" is less than "max". Both can be negative or positive.
+--
+-- Alpha/Boss, Raid Boss, Tower Boss and Predator Boss Pals are excluded.
+-- Lucky/Rare Pals are intentionally allowed to vary.
+
+return {
+    SizeVariation = {
+        XS = {
+            min = -0.15,
+            max = 0.35
+        },
+        S = {
+            min = -0.2,
+            max = 0.4
+        },
+        M = {
+            min = -0.5,
+            max = 0.4
+        },
+        L = {
+            min = -0.5,
+            max = -0.15
+        },
+        XL = {
+            min = -0.5,
+            max = -0.2
+        }
+    },
+    MinimumScale = 0.1,		
+    DebugLogging = false
+}
